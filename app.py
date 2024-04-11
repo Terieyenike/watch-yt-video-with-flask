4fcc28fbd775733e9858df3bfbb20a7366d2aa61
Oteri Eyenike <teyenike1@gmail.com>
2024-04-11 03:07:19 +0100

    Feat: refactored code for the watching youtube

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import pywhatkit
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
   return render_template("index.html")
+
+
+@app.route("/watch", methods=["POST"])
+def watch():
+   try:
+    video = request.form['video']
+    pywhatkit.playonyt(video)
+    return render_template('watch.html', video=video)
+   except Exception as e:
+     return render_template('error.html', error=str(e))
